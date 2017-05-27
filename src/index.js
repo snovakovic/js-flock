50db783f30324f4a107f989361ceff01798ba53f
snovakovic <stefan.novakovich@gmail.com>
2017-05-27 12:46:44 +0200

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-module.export.collar = require('./collar');
-module.export.deepSeal = require('./deepSeal');
-module.export.deepFreeze = require('./deepFreeze');
-module.export.promisify = require('./promisify');
-module.export.toEnum = require('./toEnum');
+exports.collar = require('./collar');
+exports.deepSeal = require('./deepSeal');
+exports.deepFreeze = require('./deepFreeze');
+exports.promisify = require('./promisify');
+exports.toEnum = require('./toEnum');
