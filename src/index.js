256326aa34593dded12bedf209eb59dfd0f1690b
snovakovic <stefan.novakovich@gmail.com>
2017-05-27 12:37:39 +0200

    Add index.js main entry file

---
+++ src/index.js
@@ -0,0 +1,5 @@
+module.export.collar = require('./collar');
+module.export.deepSeal = require('./deepSeal');
+module.export.deepFreeze = require('./deepFreeze');
+module.export.promisify = require('./promisify');
+module.export.toEnum = require('./toEnum');
