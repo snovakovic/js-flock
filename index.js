9ec37ad13a18e9d78986a8c497cf9b3bbe51c1e7
snovakovic <stefan.novakovich@gmail.com>
2017-05-27 13:22:41 +0200

    add npmignore
    move index.js to root

---
+++ index.js
@@ -0,0 +1,5 @@
+exports.collar = require('./src/collar');
+exports.deepSeal = require('./src/deepSeal');
+exports.deepFreeze = require('./src/deepFreeze');
+exports.promisify = require('./src/promisify');
+exports.toEnum = require('./src/toEnum');
--- src/index.js
+++
@@ -1,5 +0,0 @@
-exports.collar = require('./collar');
-exports.deepSeal = require('./deepSeal');
-exports.deepFreeze = require('./deepFreeze');
-exports.promisify = require('./promisify');
-exports.toEnum = require('./toEnum');
