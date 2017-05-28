3a76f6c17f8643a3d95c0a52df0c08b4e47e946f
snovakovic <stefan.novakovich@gmail.com>
2017-05-28 19:05:26 +0200

    Move comments to appropriate location

--- src/collar.js
+++ src/collar.js
@@ -1,3 +1,9 @@
+const REJECTION_REASON = Object.freeze({
+  isStrangled: true,
+  message: 'Promises have timed out'
+});
+
+
 /**
  * Set maximum waiting time for promise to resolve
  * Reject promise if it's not resolved in that time
@@ -6,12 +12,6 @@
  * @param {number} [ttl=5000] time to wait for promise to resolve
  * @returns {Promise}
  */
-
-const REJECTION_REASON = Object.freeze({
-  isStrangled: true,
-  message: 'Promises have timed out'
-});
-
 module.exports = function(promise, ttl = 5000) {
   const restraint = new Promise((resolve, reject) =>
     setTimeout(reject, ttl, REJECTION_REASON));
