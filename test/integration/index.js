dbc466ceb2afe4203a56330cf0a60ede94661f3d
Stefan Novaković <stefan.novakovich@gmail.com>
2017-06-05 09:34:04 +0200

    integration test

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,16 +1,28 @@
 const assert = require('assert');
 const exec = require('child_process').exec;
 
-
-function runTest() {
+/* eslint-disable no-console, global-require, import/no-extraneous-dependencies*/
+function run() {
   const jsFlock = require('js-flock');
 
-  // Colar
+  // Promisify
+  const promisified = jsFlock.promisify((cb) => { cb(undefined, 10); });
+  promisified().then((data) => {
+    assert.equal(data, 10);
+    console.log('Promisify: SUCCESS');
+  });
+
+  // Collar
   jsFlock.collar(Promise.resolve('test'), 5)
     .then((response) => {
       assert.equal(response, 'test');
-      console.log('COLLAR: SUCCESS');
+      console.log('Collar: SUCCESS');
     });
+
+  // toEnum
+  const testEnum = jsFlock.toEnum(['TEST']);
+  assert.equal(testEnum.TEST, 'TEST');
+  console.log('To Enum: SUCCESS');
 
   // Deep freeze
   const frozen = jsFlock.deepFreeze({ a: 1 });
@@ -21,25 +33,6 @@ function runTest() {
   const sealed = jsFlock.deepSeal({ a: 1 });
   assert.equal(Object.isSealed(sealed), true);
   console.log('Deep Seal: SUCCESS');
-
-  // toEnum
-  const testEnum = jsFlock.toEnum(['TEST']);
-  assert.equal(testEnum.TEST, 'TEST');
-  console.log('To Enum: SUCCESS');
-
-  // Promisify
-  const promisified = jsFlock.promisify((cb) => { cb(undefined, 10); });
-  promisified().then((data) => {
-    assert.equal(data, 10);
-    console.log('PROMISIFY: SUCCESS');
-  });
 }
 
-exec('npm uninstall js-flock && npm install js-flock', (error) => {
-  if (error) {
-    console.error(error);
-  }
-
-  runTest();
-});
-
+exec('npm uninstall js-flock && npm install js-flock', run);
