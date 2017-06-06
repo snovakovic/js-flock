bfacafea622114f1f349e8c947cf6aeed9cf8f9e
Stefan Novaković <stefan.novakovich@gmail.com>
2017-06-06 10:00:13 +0200

    Move index inside src

--- index.js
+++
@@ -1,5 +0,0 @@
-exports.collar = require('./src/collar');
-exports.deepSeal = require('./src/deepSeal');
-exports.deepFreeze = require('./src/deepFreeze');
-exports.promisify = require('./src/promisify');
-exports.toEnum = require('./src/toEnum');
---
+++ src/index.js
@@ -0,0 +1,5 @@
+exports.collar = require('./collar');
+exports.deepSeal = require('./deepSeal');
+exports.deepFreeze = require('./deepFreeze');
+exports.promisify = require('./promisify');
+exports.toEnum = require('./toEnum');
