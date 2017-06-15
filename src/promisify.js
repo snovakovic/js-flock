c9e7a25c2539e10d0f3009bbfcb5383086f0766d
snovakovic <stefan.novakovich@gmail.com>
2017-06-15 11:04:23 +0200

    Refactor promisify

--- src/promisify.js
+++ src/promisify.js
@@ -1,19 +1,15 @@
+const promisify = function(callbackFn, args) {
+  return new Promise((resolve, reject) => {
+    args.push((err, result) => (err ? reject(err) : resolve(result)));
+    callbackFn(...args);
+  });
+};
+
 /**
  * Promisify error first callback function
  *
- * @param {Function} fn - error first callback function we want to promisify
- * @returns {Function} Function that returns promise
+ * @param {Function} callbackFn - error first callback function we want to promisify
+ * @returns {Function} Function that returns promise when called
  */
-module.exports = function(fn) {
-  return function(...args) {
-    return new Promise((resolve, reject) => {
-      try {
-        fn.apply(this, args.concat((err, response) => (
-          err ? reject(err) : resolve(response)
-        )));
-      } catch (err) {
-        reject(err);
-      }
-    });
-  };
-};
+module.exports = (callbackFn) =>
+  (...args) => promisify(callbackFn, args);
