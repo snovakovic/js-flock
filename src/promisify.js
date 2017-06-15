9717889d6fbe2c465175fcc29f6faaca93118629
snovakovic <stefan.novakovich@gmail.com>
2017-06-15 11:56:58 +0200

    Promisify implement multiArgs handler

--- src/promisify.js
+++ src/promisify.js
@@ -1,6 +1,10 @@
-const promisify = function(callbackFn, args) {
+const promisify = function(callbackFn, args, options = {}) {
   return new Promise((resolve, reject) => {
-    args.push((err, result) => (err ? reject(err) : resolve(result)));
+    args.push((err, ...result) => {
+      if (err) return reject(err);
+      return options.multiArgs ? resolve(result) : resolve(result[0]);
+    });
+
     callbackFn(...args);
   });
 };
@@ -11,5 +15,5 @@ const promisify = function(callbackFn, args) {
  * @param {Function} callbackFn - error first callback function we want to promisify
  * @returns {Function} Function that returns promise when called
  */
-module.exports = (callbackFn) =>
-  (...args) => promisify(callbackFn, args);
+module.exports = (callbackFn, options) =>
+  (...args) => promisify(callbackFn, args, options);
--- test/promisify.spec.js
+++ test/promisify.spec.js
@@ -41,6 +41,50 @@ describe('promisify', () => {
     }).catch(shouldNotBeCalled);
   });
 
+  it('Should resolve promisified function with multiple params with multiArgs', (done) => {
+    const fun = (cb) => cb(undefined, 'res1', 2, 'res3');
+    const funAsync = promisify(fun, { multiArgs: true });
+
+    funAsync().then(([r1, r2, r3]) => {
+      expect(r1).to.equal('res1');
+      expect(r2).to.equal(2);
+      expect(r3).to.equal('res3');
+      done();
+    }).catch(shouldNotBeCalled);
+  });
+
+  it('Should resolve promisified function with multiple params without multiArgs', (done) => {
+    const fun = (cb) => cb(undefined, 'res1', 2);
+    const funAsync = promisify(fun);
+
+    funAsync().then((r1, r2) => {
+      expect(r1).to.equal('res1');
+      expect(r2).to.equal(undefined);
+      done();
+    }).catch(shouldNotBeCalled);
+  });
+
+  it('Should resolve promisified function with no params', (done) => {
+    const fun = (cb) => cb(undefined);
+    const funAsync = promisify(fun);
+
+    funAsync().then((response) => {
+      expect(response).to.equal(undefined);
+      done();
+    }).catch(shouldNotBeCalled);
+  });
+
+  it('Should resolve promisified function with no params and multiArgs args', (done) => {
+    const fun = (cb) => cb(undefined);
+    const funAsync = promisify(fun, { multiArgs: true });
+
+    funAsync().then(([res1, res2]) => {
+      expect(res1).to.equal(undefined);
+      expect(res2).to.equal(undefined);
+      done();
+    }).catch(shouldNotBeCalled);
+  });
+
   it('Should handle function that throws error', (done) => {
     const badFun = () => { throw Error('Error have happened in function'); };
     const badFunAsync = promisify(badFun);
