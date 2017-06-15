f0509c5cb1daebcc0a0e692fcb765093bfb4e089
snovakovic <stefan.novakovich@gmail.com>
2017-06-15 13:57:36 +0200

    Combine specs for deepSeal and deepFreeze

---
+++ test/deepSealAndFreeze.spec.js
@@ -0,0 +1,57 @@
+const expect = require('chai').expect;
+const deepFreeze = require('../src/deepFreeze');
+const deepSeal = require('../src/deepSeal');
+
+
+describe('deepSealOrFreeze', () => {
+  let obj;
+  let circ1;
+  let circ2;
+
+  beforeEach(() => {
+    obj = deepFreeze({
+      first: {
+        second: {
+          third: { num: 11, fun() {} }
+        }
+      }
+    });
+
+    circ1 = { first: { test: 1 } };
+    circ2 = { second: { test: 2 } };
+
+    // Create circular reference
+    circ2.circ1 = circ1;
+    circ1.circ2 = circ2;
+  });
+
+  describe('deepFreeze', () => {
+    it('Should deep freeze nested objects', () => {
+      expect(Object.isFrozen(obj.first.second)).to.equal(true);
+      expect(Object.isFrozen(obj.first.second.third)).to.equal(true);
+      expect(Object.isFrozen(obj.first.second.third.fun)).to.equal(true);
+    });
+
+    it('Should handle circular reference', () => {
+      deepFreeze(circ1);
+      expect(Object.isFrozen(circ1.first)).to.equal(true);
+      expect(Object.isFrozen(circ1.circ2)).to.equal(true);
+      expect(Object.isFrozen(circ1.circ2.second)).to.equal(true);
+    });
+  });
+
+  describe('deepSeal', () => {
+    it('Should deep seal nested objects', () => {
+      expect(Object.isSealed(obj.first.second)).to.equal(true);
+      expect(Object.isSealed(obj.first.second.third)).to.equal(true);
+      expect(Object.isSealed(obj.first.second.third.fun)).to.equal(true);
+    });
+
+    it('Should handle circular reference', () => {
+      deepSeal(circ1);
+      expect(Object.isSealed(circ1.first)).to.equal(true);
+      expect(Object.isSealed(circ1.circ2)).to.equal(true);
+      expect(Object.isSealed(circ1.circ2.second)).to.equal(true);
+    });
+  });
+});
