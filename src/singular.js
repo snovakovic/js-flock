6de83888f5dc66e7e5f734ef5a23870b950ad9b4
Stefan Novaković <stefan.novakovich@gmail.com>
2017-06-28 10:37:04 +0200

    Add documentation for singular

--- src/singular.js
+++ src/singular.js
@@ -1,3 +1,10 @@
+/**
+ * Creates singular function that after is called can't be called again until it finishes with execute.
+ * Singular functions injects done function as a first argument of original function. When call done indicates that function can be called again.
+ *
+ * @param {Function} fn - function which execution we want to control
+ * @returns {Function} Function with controlled execution
+ */
 module.exports = function(fn) {
   let inProgress = false;
   const done = () => (inProgress = false);
