18387e5c5e10d22cc8a7526375ac7bacfc347e06
Stefan Novaković <stefan.novakovich@gmail.com>
2017-07-04 09:50:54 +0200

    toEnum: Update error messages.

--- test/toEnum.spec.js
+++ test/toEnum.spec.js
@@ -79,7 +79,7 @@ describe('toEnum', () => {
   });
 
   it('Should throw exception on invalid enum', () => {
-    const notObjectOrArray = 'toEnum: Provided value needs to be object or array';
+    const notObjectOrArray = 'toEnum: Provided value need to be object or array';
     expect(() => toEnum(33)).to.throw(TypeError, notObjectOrArray);
     expect(() => toEnum(null)).to.throw(TypeError, notObjectOrArray);
     expect(() => toEnum(['first', 33])).to.throw(TypeError,
