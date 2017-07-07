41b06fccd6a46035d31afd3bbfd6e7293265e0a7
snovakovic <stefan.novakovich@gmail.com>
2017-07-07 19:17:23 +0200

    remove dist from git

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-exports.collar = require('./collar');
-exports.deepFreeze = require('./deepFreeze');
-exports.deepSeal = require('./deepSeal');
-exports.promisify = require('./promisify');
-exports.singular = require('./singular');
-exports.sort = require('./sort');
-exports.toEnum = require('./toEnum');
