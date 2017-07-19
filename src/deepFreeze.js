74bd377eb3f65252c203460b21e0d520152659c5
Stefan Novaković <stefan.novakovich@gmail.com>
2017-07-19 17:02:34 +0200

    Added dash

--- src/deepFreeze.js
+++ src/deepFreeze.js
@@ -3,7 +3,7 @@ const deep = require('./.internals/deep');
 /**
 * Recursively apply Object.freez.
 *
-* @param {Object} obj object that will be frozen including all child object/functions
+* @param {Object} obj - object that will be frozen including all child object/functions
 * @returns {Object} frozen object
 */
 module.exports = (obj, options) => deep('freeze', obj, options);
