8ed8319b8a5fdd713055b539fc8f5f83e257bcaa
Stefan Novaković <stefan.novakovich@gmail.com>
2017-07-21 14:48:26 +0200

    Moved isPlain object to root

--- src/internals/isPlainObject.js
+++
@@ -1,8 +0,0 @@
-// Public
-
-module.exports = function(testVar) {
-  if (!testVar || typeof testVar !== 'object') {
-    return false;
-  }
-  return Object.prototype.toString.call(testVar) === '[object Object]';
-};
