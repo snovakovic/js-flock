41ecbc332f086bdb13eb87fa85899c867dddfa37
Stefan Novaković <stefan.novakovich@gmail.com>
2017-09-22 15:18:03 +0200

    Promisify benchmark (#27)
    * Promisify benchmark
    
    * Add promisify benchmark to readme
    
    * Eslint disable no console in benchmark
    
    * Remove comments

---
+++ benchmark/promisify/index.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+
+const Benchmark = require('benchmark');
+const Bluebird = require('bluebird');
+const flockPromisify = require('js-flock/promisify');
+const nodePromisify = require('util').promisify;
+
+
+function promisifyBenchmark() {
+  return new Promise((resolve) => {
+    const testFun = function(cb) {
+      setTimeout(() => cb(null, 'done'), 100);
+    };
+
+    console.log('Promisify benchmark: \n');
+
+    new Benchmark.Suite()
+      .add('js-flock', () => { flockPromisify(testFun); })
+      .add('bluebird', () => { Bluebird.promisify(testFun); })
+      .add('node-util', () => { nodePromisify(testFun); })
+      .on('cycle', (event) => { console.log(String(event.target)); })
+      .on('complete', function() {
+        console.log(`\n Fastest is ${this.filter('fastest').map('name')} \n`);
+        resolve();
+      })
+      .run({ async: true });
+  });
+}
+
+function promisifyAllBenchmark() {
+  const testObj = {
+    fn1(cb) { setTimeout(() => cb(null, 'fn1'), 100); },
+    fn2(cb) { setTimeout(() => cb(null, 'fn2'), 100); },
+    fn3(cb) { setTimeout(() => cb(null, 'fn3'), 100); },
+    fn4(cb) { setTimeout(() => cb(null, 'fn4'), 100); }
+  };
+
+  console.log('\nPromisify all benchmark: \n');
+
+  new Benchmark.Suite()
+    .add('js-flock', () => { flockPromisify.all(Object.assign({}, testObj)); })
+    .add('bluebird', () => { Bluebird.promisifyAll(testObj); })
+    .on('cycle', (event) => { console.log(String(event.target)); })
+    .on('complete', function() {
+      console.log(`\n Fastest is ${this.filter('fastest').map('name')}`);
+    })
+    .run({ async: true });
+}
+
+const version = parseFloat(process.version.replace('v', ''));
+
+if (version < 8) {
+  console.warn('NODE version 8 or higher required to run this benchmark');
+} else {
+  promisifyBenchmark().then(promisifyAllBenchmark);
+}
