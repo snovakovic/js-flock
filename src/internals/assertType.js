40832f7fa99fc013d5e8524fbb5e7682857843ed
Stefan Novaković <stefan.novakovich@gmail.com>
2017-10-02 15:00:26 +0200

    remove getTag helper

--- src/internals/assertType.js
+++ src/internals/assertType.js
@@ -1,9 +1,7 @@
-const getTag = require('./getTag');
-
 // Public
 
 module.exports = (moduleName) => (type, val) => {
-  const tag = getTag(val);
+  const tag = Object.prototype.toString.call(val);
   if (`[object ${type}]` !== tag) {
     throw new TypeError(`${moduleName}: expected [${type}] but got ${tag}]`);
   }
