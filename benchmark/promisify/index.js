300cd235868a85c4e675e16b82bee5240571bf70
Stefan Novaković <stefan.novakovich@gmail.com>
2017-10-05 09:48:43 +0200

    Benchamrk updates

--- benchmark/promisify/index.js
+++ benchmark/promisify/index.js
@@ -5,13 +5,12 @@ const Bluebird = require('bluebird');
 const flockPromisify = require('js-flock/promisify');
 const nodePromisify = require('util').promisify;
 
+const testFun = function(cb) {
+  setTimeout(() => cb(null, 'done'), 0);
+};
 
-function promisifyBenchmark() {
+const promisifyBenchmark = function() {
   return new Promise((resolve) => {
-    const testFun = function(cb) {
-      setTimeout(() => cb(null, 'done'), 100);
-    };
-
     console.log('Promisify benchmark: \n');
 
     new Benchmark.Suite()
@@ -25,32 +24,72 @@ function promisifyBenchmark() {
       })
       .run({ async: true });
   });
-}
+};
 
-function promisifyAllBenchmark() {
+const promisifyAllBenchmark = function() {
   const testObj = {
-    fn1(cb) { setTimeout(() => cb(null, 'fn1'), 100); },
-    fn2(cb) { setTimeout(() => cb(null, 'fn2'), 100); },
-    fn3(cb) { setTimeout(() => cb(null, 'fn3'), 100); },
-    fn4(cb) { setTimeout(() => cb(null, 'fn4'), 100); }
+    fn1: testFun,
+    fn2: testFun,
+    fn3: testFun,
+    fn4: testFun
   };
 
   console.log('\nPromisify all benchmark: \n');
 
+  return new Promise((resolve) => {
+    new Benchmark.Suite()
+      .add('js-flock', () => { flockPromisify.all(Object.assign({}, testObj)); })
+      .add('bluebird', () => { Bluebird.promisifyAll(testObj); })
+      .on('cycle', (event) => { console.log(String(event.target)); })
+      .on('complete', function() {
+        console.log(`\n Fastest is ${this.filter('fastest').map('name')}`);
+        resolve();
+      })
+      .run({ async: true });
+  });
+};
+
+const promisifiedExecutionBenchmark = function() {
+  console.log('\nExecution of promisified functions: \n');
+
+  const flockPromisified = flockPromisify(testFun);
+  const nodePromisified = nodePromisify(testFun);
+  const bluebirdPromisified = Bluebird.promisify(testFun);
+
   new Benchmark.Suite()
-    .add('js-flock', () => { flockPromisify.all(Object.assign({}, testObj)); })
-    .add('bluebird', () => { Bluebird.promisifyAll(testObj); })
+    .add('js-flock', {
+      defer: true,
+      fn: (deferred) => {
+        flockPromisified().then(() => { deferred.resolve(); });
+      }
+    })
+    .add('node-util', {
+      defer: true,
+      fn: (deferred) => {
+        nodePromisified().then(() => { deferred.resolve(); });
+      }
+    })
+    .add('bluebird', {
+      defer: true,
+      fn: (deferred) => {
+        bluebirdPromisified().then(() => { deferred.resolve(); });
+      }
+    })
     .on('cycle', (event) => { console.log(String(event.target)); })
     .on('complete', function() {
       console.log(`\n Fastest is ${this.filter('fastest').map('name')}`);
     })
     .run({ async: true });
-}
+};
+
+// Execute benchmark
 
 const version = parseFloat(process.version.replace('v', ''));
 
 if (version < 8) {
-  console.warn('NODE version 8 or higher required to run this benchmark');
-} else {
-  promisifyBenchmark().then(promisifyAllBenchmark);
+  throw Error('NODE version 8 or higher required to run this benchmark');
 }
+
+promisifyBenchmark()
+  .then(promisifyAllBenchmark)
+  .then(promisifiedExecutionBenchmark);
