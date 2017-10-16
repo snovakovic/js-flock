a048a014f314f55d90a63885ec59a1f23b2baba1
Stefan Novaković <stefan.novakovich@gmail.com>
2017-10-16 15:55:32 +0200

    Remove space from function calls

--- src/internals/deep.js
+++ src/internals/deep.js
@@ -11,7 +11,7 @@ const isApplied = {
 module.exports = function deep(action, obj) {
   Object[action](obj);
 
-  Object.getOwnPropertyNames( obj ).forEach(( key ) => {
+  Object.getOwnPropertyNames(obj).forEach((key) => {
     const prop = obj !== Function.prototype && obj[key]; // Function.prototype is used to prevent following error on function prototype => TypeError: 'caller' and 'arguments' are restricted function properties and cannot be accessed in this context
     if (prop &&
       (typeof prop === 'object' || typeof prop === 'function') &&
