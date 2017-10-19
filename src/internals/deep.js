47374ccfa4f48011bd567e4b6791a794f91c1051
Stefan Novaković <stefan.novakovich@gmail.com>
2017-10-19 16:15:38 +0200

    Freeze Symbol properties

--- src/internals/deep.js
+++ src/internals/deep.js
@@ -11,7 +11,9 @@ const isApplied = {
 module.exports = function deep(action, obj) {
   Object[action](obj);
 
-  Object.getOwnPropertyNames(obj).forEach((key) => {
+  const keys = Object.getOwnPropertyNames(obj).concat(Object.getOwnPropertySymbols(obj));
+
+  keys.forEach((key) => {
     const prop = obj !== Function.prototype && obj[key]; // Function.prototype is used to prevent following error on function prototype => TypeError: 'caller' and 'arguments' are restricted function properties and cannot be accessed in this context
     if (prop &&
       (typeof prop === 'object' || typeof prop === 'function') &&
