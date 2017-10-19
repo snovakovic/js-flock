2e16fddc9d8c16f2002fde153da8415d35f84eb5
Stefan Novaković <stefan.novakovich@gmail.com>
2017-10-19 16:19:58 +0200

    Use ownKeys instead of concatination

--- src/internals/deep.js
+++ src/internals/deep.js
@@ -11,9 +11,7 @@ const isApplied = {
 module.exports = function deep(action, obj) {
   Object[action](obj);
 
-  const keys = Object.getOwnPropertyNames(obj).concat(Object.getOwnPropertySymbols(obj));
-
-  keys.forEach((key) => {
+  Reflect.ownKeys(obj).forEach((key) => {
     const prop = obj !== Function.prototype && obj[key]; // Function.prototype is used to prevent following error on function prototype => TypeError: 'caller' and 'arguments' are restricted function properties and cannot be accessed in this context
     if (prop &&
       (typeof prop === 'object' || typeof prop === 'function') &&
