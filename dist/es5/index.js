6af676f2a82b7520884e554c7b4b3a87c4691f48
snovakovic <stefan.novakovich@gmail.com>
2017-10-19 23:10:01 +0200

    v3.0.3

--- dist/es5/index.js
+++ dist/es5/index.js
@@ -25,27 +25,24 @@ var collar = function collar(promise) {
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-// Internals
-
-var isApplied = {
-  freeze: Object.isFrozen,
-  seal: Object.isSealed,
-  preventExtensions: function preventExtensions(prop) {
-    return !Object.isExtensible(prop);
-  }
-};
-
 // Public
 
 var deep = function deep(action, obj) {
+  var processed = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : new Set();
+
   Object[action](obj);
 
-  Reflect.ownKeys(obj).forEach(function (key) {
-    var prop = obj !== Function.prototype && obj[key]; // Function.prototype is used to prevent following error on function prototype => TypeError: 'caller' and 'arguments' are restricted function properties and cannot be accessed in this context
-    if (prop && ((typeof prop === 'undefined' ? 'undefined' : _typeof(prop)) === 'object' || typeof prop === 'function') && !ArrayBuffer.isView(prop) && !isApplied[action](prop)) {
-      deep(action, prop);
-    }
-  });
+  processed.add(obj); // Prevent circular reference
+
+  if (obj !== Function.prototype) {
+    // Prevent TypeError: 'caller' and 'arguments' are restricted function properties and cannot be accessed in this context
+    Reflect.ownKeys(obj).forEach(function (key) {
+      var prop = obj[key];
+      if (prop && ((typeof prop === 'undefined' ? 'undefined' : _typeof(prop)) === 'object' || typeof prop === 'function') && !ArrayBuffer.isView(prop) && !processed.has(prop)) {
+        deep(action, prop, processed);
+      }
+    });
+  }
 
   return obj;
 };
