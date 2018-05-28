19d7b20037f41da1bf5771e7dc753094c47958d0
snovakovic <stefan.novakovich@gmail.com>
2018-05-28 14:21:24 +0200

    v3.3.2

--- dist/es5/deepSeal.js
+++ dist/es5/deepSeal.js
@@ -29,20 +29,22 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 
 /**
  * Recursively apply provided operation on object and all of the object properties that are either object or function.
- *
- * @param {string='freeze', 'seal', 'preventExtensions'} action - The action to be applied on object and his properties
- * @param {Object} obj - The object we want to modify
- * @param {Object} [options]
+ * @param {string['freeze', 'seal', 'preventExtensions']} action - The action to be applied on object and his properties
+ * @param {Object} obj - The object that will be deeply freeze/seal...
+ * @param {Object} [options] - Optional options that controls what will be affected with deep acion
  * @param {boolean} [options.proto=false] - Should we loop over prototype chain or not
+ * @param {boolean} [options.exclude=Function] - Function that decide should propery be excluded or included.
+ * Function accepts key as first parametar and property context(object/function) as second parameter
  * @param {Set} [processed=new Set()] - Used internally to prevent circular references
- *
- *  @returns {Object} Initial object which now have applied actions on him
+ * @returns {Object} Initial object with aplied action(freeze/seel/preventExtension) on it
  */
 var deep = function deep(action, obj, options) {
   var processed = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : new Set();
 
   // Prevent circular reference
   if (processed.has(obj)) return obj;
+
+  options = options || {};
 
   Object[action](obj);
   processed.add(obj);
@@ -59,14 +61,14 @@ var deep = function deep(action, obj, options) {
 
   ownKeys.forEach(function (key) {
     var prop = obj[key];
-    if (prop && ((typeof prop === 'undefined' ? 'undefined' : _typeof(prop)) === 'object' || typeof prop === 'function') && typeof ArrayBuffer !== 'undefined' && !ArrayBuffer.isView(prop)) {
-      // Prevent issue with freezing buffers
+    if (prop && ((typeof prop === 'undefined' ? 'undefined' : _typeof(prop)) === 'object' || typeof prop === 'function') && typeof ArrayBuffer !== 'undefined' && !ArrayBuffer.isView(prop) && ( // Prevent issue with freezing buffers
+    typeof options.exclude !== 'function' || !options.exclude(key, obj))) {
       deep(action, prop, options, processed);
     }
   });
 
   // Freeze object prototype if specified
-  if (options && (typeof options === 'undefined' ? 'undefined' : _typeof(options)) === 'object' && options.proto) {
+  if (options.proto) {
     var proto = Object.getPrototypeOf(obj);
     if (proto && !isNativeObject(proto)) {
       deep(action, proto, options, processed);
--- dist/internals/deep.js
+++ dist/internals/deep.js
@@ -4,18 +4,20 @@ const isNativeObject = require('./isNativeObject');
 
 /**
  * Recursively apply provided operation on object and all of the object properties that are either object or function.
- *
- * @param {string='freeze', 'seal', 'preventExtensions'} action - The action to be applied on object and his properties
- * @param {Object} obj - The object we want to modify
- * @param {Object} [options]
+ * @param {string['freeze', 'seal', 'preventExtensions']} action - The action to be applied on object and his properties
+ * @param {Object} obj - The object that will be deeply freeze/seal...
+ * @param {Object} [options] - Optional options that controls what will be affected with deep acion
  * @param {boolean} [options.proto=false] - Should we loop over prototype chain or not
+ * @param {boolean} [options.exclude=Function] - Function that decide should propery be excluded or included.
+ * Function accepts key as first parametar and property context(object/function) as second parameter
  * @param {Set} [processed=new Set()] - Used internally to prevent circular references
- *
- *  @returns {Object} Initial object which now have applied actions on him
+ * @returns {Object} Initial object with aplied action(freeze/seel/preventExtension) on it
  */
 module.exports = function deep(action, obj, options, processed = new Set()) {
   // Prevent circular reference
   if (processed.has(obj)) return obj;
+
+  options = options || {};
 
   Object[action](obj);
   processed.add(obj);
@@ -34,13 +36,14 @@ module.exports = function deep(action, obj, options, processed = new Set()) {
     const prop = obj[key];
     if (prop &&
       (typeof prop === 'object' || typeof prop === 'function') &&
-      (typeof ArrayBuffer !== 'undefined' && !ArrayBuffer.isView(prop))) { // Prevent issue with freezing buffers
+      (typeof ArrayBuffer !== 'undefined' && !ArrayBuffer.isView(prop)) && // Prevent issue with freezing buffers
+      (typeof options.exclude !== 'function' || !options.exclude(key, obj))) {
       deep(action, prop, options, processed);
     }
   });
 
   // Freeze object prototype if specified
-  if (options && typeof options === 'object' && options.proto) {
+  if (options.proto) {
     const proto = Object.getPrototypeOf(obj);
     if (proto && !isNativeObject(proto)) {
       deep(action, proto, options, processed);
