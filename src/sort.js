571177ff96ed8291c3ae84e998ddeaa0cd73ede8
snovakovic <stefan.novakovich@gmail.com>
2018-06-03 13:00:43 +0200

    Allow sorting in multiple directions

--- src/sort.js
+++ src/sort.js
@@ -10,9 +10,9 @@ const sorter = function(direction, a, b) {
 };
 
 /**
- * String sorter does not support nested property.
+ * stringSorter does not support nested property.
  * For nested properties or value transformation (e.g toLowerCase) we should use functionSorter
- * Based on benchmark testing using string sorter is bit faster then using equivalent function sorter
+ * Based on benchmark testing using stringSorter is bit faster then using equivalent function sorter
  * @example sort(users).asc('firstName')
  */
 const stringSorter = function(direction, sortBy, a, b) {
@@ -27,22 +27,26 @@ const functionSorter = function(direction, sortBy, a, b) {
 };
 
 /**
- * Return string or function sorter depending on sortBy value
+ * Return multiProperty sort handler
  * @param {Function, String} sortBy
  * @returns {Function} sorter
  */
-const getMultyPropertySort = function(sortBy) {
-  if (typeof sortBy === 'string') {
-    return stringMultyPropertySort; // eslint-disable-line no-use-before-define
+const getMultiPropertySorter = function(sortBy) {
+  const type = typeof sortBy;
+  if (type === 'string') {
+    return stringMultiPropertySort; // eslint-disable-line no-use-before-define
+  } else if (type === 'function') {
+    return functionMultiPropertySort; // eslint-disable-line no-use-before-define
   }
 
-  return functionMultyPropertySort; // eslint-disable-line no-use-before-define
+  return objectMultiPropertySort; // eslint-disable-line no-use-before-define
 };
 
-const multyPropertySort = function(valA, valB, direction, thenBy, depth, a, b) {
-  if (valA === valB) {
+const multiPropertySort = function(valA, valB, direction, thenBy, depth, a, b) {
+  if (valA === valB || (valA == null && valB == null)) {
     if (thenBy.length > depth) {
-      return getMultyPropertySort(thenBy[depth])(direction, thenBy[depth], thenBy, depth + 1, a, b);
+      const multiSorter = getMultiPropertySorter(thenBy[depth]);
+      return multiSorter(thenBy[depth], thenBy, depth + 1, direction, a, b);
     }
     return 0;
   }
@@ -54,16 +58,34 @@ const multyPropertySort = function(valA, valB, direction, thenBy, depth, a, b) {
  * Used when we have sorting by multyple properties and when current sorter is function
  * @example sort(users).asc([p => p.address.city, p => p.firstName])
  */
-const functionMultyPropertySort = function(direction, sortBy, thenBy, depth, a, b) {
-  return multyPropertySort(sortBy(a), sortBy(b), direction, thenBy, depth, a, b);
+const functionMultiPropertySort = function(sortBy, thenBy, depth, direction, a, b) {
+  return multiPropertySort(sortBy(a), sortBy(b), direction, thenBy, depth, a, b);
 };
 
 /**
  * Used when we have sorting by multyple properties and when current sorter is string
  * @example sort(users).asc(['firstName', 'lastName'])
  */
-const stringMultyPropertySort = function(direction, sortBy, thenBy, depth, a, b) {
-  return multyPropertySort(a[sortBy], b[sortBy], direction, thenBy, depth, a, b);
+const stringMultiPropertySort = function(sortBy, thenBy, depth, direction, a, b) {
+  return multiPropertySort(a[sortBy], b[sortBy], direction, thenBy, depth, a, b);
+};
+
+/**
+ * Used with 'by' sorter when we have sorting in multiple direction
+ * @example sort(users).asc(['firstName', 'lastName'])
+ */
+const objectMultiPropertySort = function(sortByObj, thenBy, depth, _direction, a, b) {
+  // TODO direction is not used remove
+  const sortBy = sortByObj.asc || sortByObj.desc;
+  const direction = sortByObj.asc ? 1 : -1;
+
+  if (!sortBy) {
+    throw Error(`sort: Invalid 'by' sorting onfiguration.
+      Expecting object with 'asc' or 'desc' key`);
+  }
+
+  const multiSorter = getMultiPropertySorter(sortBy);
+  return multiSorter(sortBy, thenBy, depth, direction, a, b);
 };
 
 /**
@@ -85,10 +107,11 @@ const sort = function(direction, ctx, sortBy) {
     _sorter = sorter.bind(undefined, direction);
   } else if (typeof sortBy === 'string') {
     _sorter = stringSorter.bind(undefined, direction, sortBy);
-  } else if (Array.isArray(sortBy)) {
-    _sorter = getMultyPropertySort(sortBy[0]).bind(undefined, direction, sortBy.shift(), sortBy, 0);
-  } else {
+  } else if (typeof sortBy === 'function') {
     _sorter = functionSorter.bind(undefined, direction, sortBy);
+  } else {
+    _sorter = getMultiPropertySorter(sortBy[0])
+      .bind(undefined, sortBy.shift(), sortBy, 0, direction);
   }
 
   return ctx.sort(_sorter);
@@ -99,6 +122,18 @@ const sort = function(direction, ctx, sortBy) {
 module.exports = function(ctx) {
   return {
     asc: (sortBy) => sort(1, ctx, sortBy),
-    desc: (sortBy) => sort(-1, ctx, sortBy)
+    desc: (sortBy) => sort(-1, ctx, sortBy),
+    by: (sortBy) => {
+      if (!Array.isArray(ctx)) return ctx;
+
+      if (!Array.isArray(sortBy) || sortBy.length < 2) {
+        throw Error(`sort: Invalid usage of 'by' sorter.
+          'by' sorter should be used only for sorting in multiple directions.
+          Did you mean to use 'asc' or 'desc' sorter instead?`);
+      }
+
+      const _sorter = objectMultiPropertySort.bind(undefined, sortBy.shift(), sortBy, 0, undefined);
+      return ctx.sort(_sorter);
+    }
   };
 };
--- test/unit/sort.spec.js
+++ test/unit/sort.spec.js
@@ -3,10 +3,16 @@ const { expect } = require('chai');
 const sort = require('../../src/sort');
 
 
-describe('sort', () => {
+describe.only('sort', () => {
   let flatArray;
   let persons;
   let multiPropArray;
+
+  function assertOrder(order, valueCb) {
+    order.forEach((item, idx) => {
+      expect(valueCb(idx)).to.equal(item);
+    });
+  }
 
   beforeEach(() => {
     flatArray = [1, 5, 3, 2, 4, 5];
@@ -35,7 +41,7 @@ describe('sort', () => {
       age: 8
     }, {
       name: 'aa',
-      lastName: undefined,
+      lastName: null,
       age: 9
     }, {
       name: 'aa',
@@ -60,28 +66,20 @@ describe('sort', () => {
 
   it('Should sort by object property in ascending order', () => {
     sort(persons).asc((p) => p.name.toLowerCase());
-    expect(persons[0].name).to.equal('FIRST');
-    expect(persons[1].name).to.equal('In the middle');
-    expect(persons[2].name).to.equal('last');
+    assertOrder(['FIRST', 'In the middle', 'last'], (idx) => persons[idx].name);
   });
 
-  it('Should sort by object property in ascending order', () => {
+  it('Should sort by object property in descending order', () => {
     sort(persons).desc((p) => p.name.toLowerCase());
-    expect(persons[2].name).to.equal('FIRST');
-    expect(persons[1].name).to.equal('In the middle');
-    expect(persons[0].name).to.equal('last');
+    assertOrder(['last', 'In the middle', 'FIRST'], (idx) => persons[idx].name);
   });
 
   it('Should sort undefined values to the bottom', () => {
     sort(persons).asc((p) => p.address.code);
-    expect(persons[0].address.code).to.equal(1);
-    expect(persons[1].address.code).to.equal(3);
-    expect(persons[2].address.code).to.equal(undefined);
+    assertOrder([1, 3, undefined], (idx) => persons[idx].address.code);
 
     sort(persons).desc((p) => p.address.code);
-    expect(persons[0].address.code).to.equal(3);
-    expect(persons[1].address.code).to.equal(1);
-    expect(persons[2].address.code).to.equal(undefined);
+    assertOrder([3, 1, undefined], (idx) => persons[idx].address.code);
 
     const sorted = sort([1, undefined, 3, null, 2]).desc();
     expect(sorted).to.eql([3, 2, 1, null, undefined]);
@@ -97,9 +95,12 @@ describe('sort', () => {
 
   it('Should sort dates correctly', () => {
     sort(persons).asc('dob');
-    expect(persons[0].name).to.equal('FIRST');
-    expect(persons[1].name).to.equal('In the middle');
-    expect(persons[2].name).to.equal('last');
+    assertOrder(['FIRST', 'In the middle', 'last'], (idx) => persons[idx].name);
+  });
+
+  it('Should should unwrap single array value', () => {
+    sort(persons).asc(['name']);
+    assertOrder(['FIRST', 'In the middle', 'last'], (idx) => persons[idx].name);
   });
 
   it('Should sort on multiple properties', () => {
@@ -109,36 +110,17 @@ describe('sort', () => {
       (p) => p.age
     ]);
 
-    expect(multiPropArray[0].age).to.equal(10);
-    expect(multiPropArray[1].age).to.equal(11);
-    expect(multiPropArray[2].age).to.equal(8);
-    expect(multiPropArray[3].age).to.equal(9);
-    expect(multiPropArray[4].age).to.equal(6);
+    assertOrder([10, 11, 8, 9, 6], (idx) => multiPropArray[idx].age);
   });
 
   it('Should sort on multiple properties by string sorter', () => {
     sort(multiPropArray).asc(['name', 'age', 'lastName']);
-    expect(multiPropArray[0].age).to.equal(8);
-    expect(multiPropArray[1].age).to.equal(9);
-    expect(multiPropArray[2].age).to.equal(10);
-    expect(multiPropArray[3].age).to.equal(11);
-    expect(multiPropArray[4].age).to.equal(6);
+    assertOrder([8, 9, 10, 11, 6], (idx) => multiPropArray[idx].age);
   });
 
   it('Should sort on multiple mixed properties', () => {
     sort(multiPropArray).asc(['name', (p) => p.lastName, 'age']);
-    expect(multiPropArray[0].age).to.equal(10);
-    expect(multiPropArray[1].age).to.equal(11);
-    expect(multiPropArray[2].age).to.equal(8);
-    expect(multiPropArray[3].age).to.equal(9);
-    expect(multiPropArray[4].age).to.equal(6);
-  });
-
-  it('Should should unwrap single array value', () => {
-    sort(persons).asc(['name']);
-    expect(persons[0].name).to.equal('FIRST');
-    expect(persons[1].name).to.equal('In the middle');
-    expect(persons[2].name).to.equal('last');
+    assertOrder([10, 11, 8, 9, 6], (idx) => multiPropArray[idx].age);
   });
 
   it('Should sort with all equal values', () => {
@@ -152,5 +134,34 @@ describe('sort', () => {
 
     sort(same).asc(['name', 'age']);
     expect(same[0].name).to.equal('a');
+  });
+
+  it('Should sort by desc name and asc lastName', () => {
+    sort(multiPropArray).by([
+      { desc: 'name' },
+      { asc: 'lastName' }
+    ]);
+
+    assertOrder(['aa', 'aa', 'bb', undefined, null], (idx) => multiPropArray[idx].lastName);
+  });
+
+  it('Should sort by asc name and desc age', () => {
+    sort(multiPropArray).by([
+      { asc: 'name' },
+      { desc: 'age' }
+    ]);
+
+    assertOrder([11, 10, 9, 8, 6], (idx) => multiPropArray[idx].age);
+  });
+
+
+  it('Should sort by asc lastName, desc name and asc age', () => {
+    sort(multiPropArray).by([
+      { asc: (p) => p.lastName },
+      { desc: (p) => p.name },
+      { asc: (p) => p.age }
+    ]);
+
+    assertOrder([6, 10, 11, 8, 9], (idx) => multiPropArray[idx].age);
   });
 });
