24a5647cb9209e75d62d384cbe74aa183df82e93
Stefan Novaković <stefan.novakovich@gmail.com>
2019-02-08 00:33:14 +0700

    Refactor collar

--- test/utils.js
+++ test/utils.js
@@ -1,7 +1,3 @@
-module.exports.shouldNotBeCalled = function() {
-  throw Error('Should not be called');
-};
-
 module.exports.delay = function(numberOfMs) {
   return new Promise((resolve) => {
     setTimeout(resolve, numberOfMs);
