d92df30446d0f7e2ab28115c52001a710232c98a
snovakovic <stefan.novakovich@gmail.com>
2019-09-04 17:54:07 +0200

    v3.11.0

---
+++ dist/es5/castBoolean.full.js
@@ -0,0 +1,18 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = global || self, (global.castBoolean = global.castBoolean || {}, global.castBoolean.js = factory()));
+}(this, function () { 'use strict';
+
+  // >>> PUBLIC <<<
+
+  /**
+   * Cast any value to boolean value
+   */
+  var castBoolean = function castBoolean(val) {
+    return val === true || val === 'true';
+  };
+
+  return castBoolean;
+
+}));
