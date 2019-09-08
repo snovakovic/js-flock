d87a331c06fe0f356c135f143a3031ad937645e6
snovakovic <stefan.novakovich@gmail.com>
2019-09-08 21:52:28 +0200

    Convert last to TS

---
+++ src/last.ts
@@ -0,0 +1,17 @@
+// >>> PUBLIC <<<
+
+function a<T>(
+  arr:T[],
+  predicate?:(item:T) => boolean,
+):T|undefined {
+  let length = Array.isArray(arr) ? arr.length : 0;
+
+  if (!length) return undefined;
+  if (typeof predicate !== 'function') return arr[length - 1];
+
+  while (--length) {
+    if (predicate(arr[length])) return arr[length];
+  }
+
+  return undefined;
+};
